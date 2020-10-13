e16e98a5b2914892f5e9984e7df819e119ec972d
Trufanov Roman <pingvin1308@gmail.com>
2020-10-13 10:15:09 +0300

    init

---
+++ RazorPages/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RazorPages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
